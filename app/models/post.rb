25d2ee91fe61ccec72c9797b2320794f5d619589
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-10-02 13:17:27 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        titles = [/Won't Believe/i,/Secret/i,/Top \d/i,/Guess/i] 
+        if titles.none? {|phrase| phrase.match title } 
+            errors.add(:title, "Not a valid title")
+        end
+    end
 end
